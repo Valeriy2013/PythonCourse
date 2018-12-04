78a7b5a0a43ad98a2cbed92d13f933ecf2724e14
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2018-12-04 14:30:28 +0200

    add tests for update and search

--- PythonRequests/jira_requests.py
+++ PythonRequests/jira_requests.py
@@ -3,34 +3,43 @@ JiraRequests.
 """
 
 import requests
+import os
 
-user = 'mail@gmail.com'
-api_key = 'key'
+user = os.env("mail")
+api_key = os.env("api_key")
 server = 'https://jira-valeriy.atlassian.net'
-data = '/rest/api/2/issue'
+issue_endpoint = '/rest/api/2/issue'
+search_endpoint = '/rest/api/2/search'
 
 
 def get_issue_by_id(key, fields='*all'):
     if fields != '*all':
-        response = requests.get(server + data + '/' + key + '?fields=' + fields, auth=(user, api_key))
+        response = requests.get(server + issue_endpoint + '/' + key + '?fields=' + fields, auth=(user, api_key))
     else:
-        response = requests.get(server + data + '/' + key, auth=(user, api_key))
+        response = requests.get(server + issue_endpoint + '/' + key, auth=(user, api_key))
     return response.status_code, response.json()
 
 
 def create_issue(issue):
-    response = requests.post(server + data, json=issue, auth=(user, api_key))
+    response = requests.post(server + issue_endpoint, json=issue, auth=(user, api_key))
+    return response.status_code, response.json()
+
+
+def create_issue_bulk(issue):
+    response = requests.post(server + issue_endpoint + '/bulk', json=issue, auth=(user, api_key))
     return response.status_code, response.json()
 
 
 def delete_issue(key):
-    response = requests.delete(server + data + '/' + key, auth=(user, api_key))
+    response = requests.delete(server + issue_endpoint + '/' + key, auth=(user, api_key))
     return response.status_code
 
 
 def update_issue(key, issue):
-    pass
+    response = requests.put(server + issue_endpoint + '/' + key, json=issue, auth=(user, api_key))
+    return response.status_code
 
 
 def search_issue(criteria):
-    pass
+    response = requests.post(server + search_endpoint, json=criteria, auth=(user, api_key))
+    return response.status_code, response.json()
--- PythonRequests/test_jira_requests.py
+++ PythonRequests/test_jira_requests.py
@@ -7,6 +7,7 @@ from PythonRequests import json_fixtures, jira_requests
 class TestJiraRequests:
 
     def test_get_issue_by_id(self):
+        # get already existing issue
         status_code, response = jira_requests.get_issue_by_id('TES-1', 'summary,description')
         assert status_code == 200
         assert response['fields']['summary'] == 'test bug 1 summary'
@@ -30,7 +31,7 @@ class TestJiraRequests:
         jira_requests.delete_issue(new_key)
 
     def test_delete_issue(self):
-        # crate issue for deletion
+        # create issue for deletion
         path = "D:\\_Projects\\PythonCourse\\PythonRequests\\jsons\\issue_to_create.json"
         issue_to_create = json_fixtures.get_json_data(path)
         status_code, response = jira_requests.create_issue(issue_to_create)
@@ -45,7 +46,46 @@ class TestJiraRequests:
         assert get_status_code == 404
 
     def test_update_issue(self):
-        pass
+        # create issue for update
+        path = "D:\\_Projects\\PythonCourse\\PythonRequests\\jsons\\issue_to_create.json"
+        issue_to_create = json_fixtures.get_json_data(path)
+        status_code, response = jira_requests.create_issue(issue_to_create)
+
+        # update issue
+        new_key = response['key']
+        path = "D:\\_Projects\\PythonCourse\\PythonRequests\\jsons\\issue_to_update.json"
+        issue_to_update = json_fixtures.get_json_data(path)
+        upd_code = jira_requests.update_issue(new_key, issue_to_update)
+        assert upd_code == 204
+
+        # get updated issue and check fields
+        get_status_code, get_response = jira_requests.get_issue_by_id(new_key, 'summary,description')
+        assert get_status_code == 200
+        assert get_response['fields']['summary'] == "Updated via REST"
+        assert get_response['fields']['description'] == "Updated description"
+
+        # delete updated issue
+        jira_requests.delete_issue(new_key)
 
     def test_search_issue(self):
-        pass
+        # create issue for search using bulk create
+        path = "D:\\_Projects\\PythonCourse\\PythonRequests\\jsons\\issues_to_create_for_search.json"
+        issues_to_create = json_fixtures.get_json_data(path)
+        status_code, response = jira_requests.create_issue_bulk(issues_to_create)
+
+        # retrieve created issues keys from response
+        keys = []
+        keys_length = len(response["issues"])
+        for i in range(0, keys_length):
+            keys.append(response["issues"][i]["key"])
+
+        # search created issues
+        path = "D:\\_Projects\\PythonCourse\\PythonRequests\\jsons\\search_criteria.json"
+        search_criteria = json_fixtures.get_json_data(path)
+        search_status_code, search_response = jira_requests.search_issue(search_criteria)
+        assert search_status_code == 200
+
+        # delete created issues
+        jira_requests.delete_issue(keys[0])
+        jira_requests.delete_issue(keys[1])
+
